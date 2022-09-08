08d2f4f0001b491f3ece0164a0e7662c110a72c6
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-09-08 19:27:34 +0000

    c programs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/*
+ * The program will display  with proper grammar, but the outcome is a piece of
+ *art,
+ */
+#include <stdio.h>
+/**
+ * main - The program starts from here
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf(" with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+/* The Program will print the size of various types */
+#include <stdio.h>
+/**
+ * main - the program starts executing from here
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
