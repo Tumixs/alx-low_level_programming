c0d213629f4b9a0c0de16b6bd8277906c52a8615
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-09-13 11:07:39 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+/* The program will print _putchar and a new line */
+#include <stdio.h>
+/**
+ * main - program execution begins here
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf ("_putchar\n");
+}
