1bf7106f71aba1de6277682baced947c287ab3c4
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-09-13 21:39:07 +0000

    islower

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets in lowercase 10x
+ * Return: returns nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar('j');
+		}
+		_putchar('\n');
+	}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase characters
+ * @c: alpabet
+ * Return: returns 1 if alphabet is lower case. otherwise 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 & c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
