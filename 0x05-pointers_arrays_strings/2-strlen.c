c2486fc1cd6dbdc3ec135885e1d874790c8c447d
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-09-20 05:56:08 +0000

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * return: returns length
+ * @s: string
+ */
+int _strlen(char *s)
+{
+	int i;
+	
+	for (i = 0; s[i] != \0; i++)
+	{
+		j = i+1;
+	}
+}
