d00b0ca112b04c3b4eba04469a20af1b1d3c768c
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-09-22 13:43:39 +0000

    A funtion that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcmp - compares two string
+ * @s1: string
+ * @s2: string
+ *
+ * Return: returns 15 if s1>s2, -15 if s1<s2, 0 if s1 ==s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+
+	for (i = 0; s1[i]; i++)
+		;
+	for (j = 0; s2[j]; j++)
+		;
+	if (i > j)
+		return (15);
+	else
+		if (i < j)
+			return (-15);
+		else
+			return (0);
+}
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+
+/**
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+	
+	for (i = 0; s1[i]; i++);
+	for (j = 0; s2[j]; j++);
+	if (i > j)
+	{	
+		return (15);
+	}
+	else 
+	if (i < j)
+	{
+		return (-15);
+	}
+	else
+		return (0);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char s1[] = "first stringsssssssssssssss";
-	char s2[] = "second string";
+	char s1[] = "Hello";
+	char s2[] = "World!";
 
-	strncpy(s1, s2, 3);
-	printf("%s\n", s1);
-	printf("%s\n", s2);
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
 	return (0);
 }
