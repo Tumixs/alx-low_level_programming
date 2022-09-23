537169fc319363a82885725dd7160377a001e1bc
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-09-23 14:46:23 +0100

    A function thst reverses an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - reverse an array of string
+ * @a: array
+ * @n: size of aaray
+ */
+void reverse_array(int *a, int n)
+{
+		int i;
+
+		for (i -= n; i >= 0; i--)
+		{
+			_putchar(i);
+			_putchar(',');
+		}
+		_putchar(30);
+}
