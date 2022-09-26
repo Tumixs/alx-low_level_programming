3bb2783b0428fe11c162644c22c1dbe30c961c02
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-09-26 22:52:25 +0000

    fill a memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /* The Function filss memory with a constant byte */
 /**
+ * _memset - fills memory with constant byte
+ * @s: array
+ * @b: value to put in memory address
+ * @n: number of memory address
+ *
+ * Return: returns a pointer to the memory address
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i =0; i < n; i++)
+	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
 }
