e8e8c905b31a740ca78e5e7820b8dd5bbf42e293
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-09-26 23:22:17 +0000

    copy a memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n-bytes to memory area
+ * @dest: memory area copied to
+ * @src: source of copied bytes
+ * @n: number of bytes copied
+ *
+ * Return: returns a pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		src[i] = dest[i];
+	return (dest);
+}
