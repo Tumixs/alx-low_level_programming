12028526aec0a8d25558bfbef6f30a97de470ad0
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-09-30 20:05:11 +0000

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - program start
+ * @argc: number of commandline arguments
+ * @argv: array of pointer characater pointer to commanline argument.
+ *
+ * Return: returns 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
