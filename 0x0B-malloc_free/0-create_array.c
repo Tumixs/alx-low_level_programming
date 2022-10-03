0df3326dd608f7a6d86c3f6d50a844a7af7aa0ab
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-10-03 20:17:24 +0000

    C function for Memory allocation to copy a string

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,14 +4,26 @@
  * create_array - creates an array of chars
  * @c: used to initilaize array
  * @size: size of array
+ *
+ * Return: returns a pointer to the initialised array
  */
 char *create_array(unsigned int size, char c)
 {
-	char *arr = &c;
-
-	arr = malloc(size);
-	if (arr == NULL)
-		return (" ");
-	return (arr);
+	char *arr;
+	unsigned int i = 0;
+	
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+	if (arr != NULL)
+	{
+		while (i < size * sizeof(char))
+		{
+			*(arr + i) = c;
+			i++;
+		}
+		return (arr);
+	}
+	return (NULL);
 }
 
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+ * _strdup - copies a string to a new memory location
+ * @str: poninter to string to copy
+ *
+ * Return: returns a pointer to the new memory location containing copied string
+ */
+char *_strdup(char *str)
+{
+	int i;
+	long unsigned int j;
+	char *tmp;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (*(str + i) != '\0')
+		i++;
+	tmp = malloc(i * sizeof(char));
+	while (j < i * sizeof(char))
+	{
+		*(tmp + j) = *(str + j);
+		j++;
+	}
+	return (tmp);
+}
