867644d9784dc067ef06243e2a81d65cee3bdde0
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-10-06 18:06:04 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,4 +4,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *tmp;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	tmp = malloc(nmemb * size);
+	if (tmp == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		*((char *)tmp + i) = 0;
+	return (tmp);
 }
