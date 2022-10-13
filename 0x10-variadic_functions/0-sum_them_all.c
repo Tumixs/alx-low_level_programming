f51d4c947c355a98f21fa7dd7a57c29e84999662
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-10-13 21:44:08 +0000

    added first task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - adds up all its arguments
+ * @n: number of arguments
+ *
+ * Return: returns the sum of all its paramters 9arguments)
+ * if n is zero, returns 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arglist;
+	unsigned int i;
+	int result = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(arglist, n);
+	for (i = 0; i < n; i++)
+		result += va_arg(arglist, int);
+	va_end(arglist);
+	return (result);
+}
