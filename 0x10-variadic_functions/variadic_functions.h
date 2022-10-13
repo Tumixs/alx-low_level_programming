32e946d690f23723b6ff6a010a010a77dfb213d0
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-10-13 21:09:53 +0000

    Modified and Added new files

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef GAURD
+#define GAURD
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print-strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
