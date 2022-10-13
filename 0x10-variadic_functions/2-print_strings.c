d1482b0e18acc94ab2168ff8b6f57f1ac0d1f4ae
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-10-13 23:49:56 +0000

    modified

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *string;
 
-	if (n == 0)
+	if (n <= 0)
 		return;
 	va_start(arglist, n);
 	for (i = 0; i < n; i++)
