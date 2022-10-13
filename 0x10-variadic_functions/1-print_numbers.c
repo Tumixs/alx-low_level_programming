295ca44a8defd9422cf174667b471f0bf4f2d9da
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-10-13 23:52:54 +0000

    modified

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (n <= 0)
 		return;
 	va_start(arglist, n);
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
 		printf("%d", va_arg(arglist, int));
 		if ((i != (n - 1)) && (separator != NULL))
