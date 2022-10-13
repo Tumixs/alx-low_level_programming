8b553e4f01cd2ac7ed7440dcbc3d19b7a5bdfcff
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-10-13 21:11:58 +0000

    modified files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,8 +21,8 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (*ops[i].op == *s)
-			return (ops[i].f);
+		if (*ops[i].op == *s) /*loops through struct to find matching operator*/
+			return (ops[i].f); /*returns function name for operator*/
 		i++;
 	}
 	return (NULL);
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,7 +1,7 @@
 # C - Variadic functions
 
 ### Mandatory Tasks
-0. [Beauty is variable, ugliness is constant] (./0-sum_them_all.c)
+0. [Beauty is variable, ugliness is constant](./0-sum_them_all.c)
 	* A function that returns the sum of all its parameters.	
 
 ## Resources
