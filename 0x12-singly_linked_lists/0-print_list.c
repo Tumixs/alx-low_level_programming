abcdf7e15bb627099ab17aa8bef973a84aebc9f5
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-11-12 02:52:50 +0000

    Task 0: Add the complete files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,24 @@
 #include "lists.h"
 
 /**
+ * print_list - prints all the elements of a list_t
+ * @h: list start
+ * Description: if string is NULL it prints nil
+ * Return: returns the number of nodes
  */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+	const struct list_s *next = h;
+
+	while (next != NULL)
+	{
+		if (next->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", next->len, next->str);
+		i++;
+		next = next->next;
+	}
+	return (i);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,25 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+int _putchar(char c);
 size_t print_list(const list_t *h);
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: lenght of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 
 #endif
 
