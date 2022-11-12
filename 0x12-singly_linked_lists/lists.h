dc01b45244edb2f7ebcdc28c8a27e1f9d98cbdc6
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-11-12 02:58:36 +0000

    Fix betty issues

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,14 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-int _putchar(char c);
-size_t print_list(const list_t *h);
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -20,6 +12,14 @@ size_t print_list(const list_t *h);
  *
  * Description: singly linked list node structure
  */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
 
