37a969a132221abd825fbace20f191da073dc8d6
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-11-15 00:26:31 +0000

    Add file for task 1 * Modify README.md, lists.h

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - determines the number of elements in
+ * in list_t
+ *@h: first node of list
+
+ * Return: returns the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return(i);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -4,5 +4,6 @@
 Title	|	Description 
 ------ 	|	---------  
 [0. Print list](./0-print_list.c) | Prints all the elements of a *list_t* list
+[1. list length](./1-list_len.c) | Returns the number of elements in a linked *list_t*.
 
 ## Resources
