5fc325d6ec4203117944bcee420ba5310432510f
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-11-15 01:05:25 +0000

    Complete task 2
    * Fix betty issue for task 1 (1-list_len.c)
    * Add file for task 2 (2-add_node.c)
    * Modify README.md, lists.h

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of *list_t*
+ * @head: first node
+ * @str: node element
+ *
+ * Return: returns the address of the new element,
+ * returns NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
