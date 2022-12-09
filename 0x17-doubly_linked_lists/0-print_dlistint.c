06f60591c7e19e150ca416803c67261a9524a41b
Asere Oluwatumise <asereoluwatumise@gmail.com>
2022-12-09 00:50:49 +0000

    Project 0x17: task 0
    * add files for task

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elments of a dlistint_t list
+ * @h: pointer to head node
+ *
+ * Return: returns the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+	const dlistint_t *ptr = h;
+
+	for (; ptr->next != NULL; ptr = ptr->next)
+	{
+		printf("%d\n", ptr->n);
+		i++;
+	}
+	printf("%d\n", ptr->n);
+	i += 1;
+	return (i);
+}
