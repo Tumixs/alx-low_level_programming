5377a288db6898b36c83d44984b74baa937e376d
Asere Oluwatumise S <asereoluwatumise@gmail.com>
2022-12-26 16:29:59 +0100

    project 0x02: task 2- Fix checker error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 'a';
+	int i;
 	int j = 0;
 
 	while (j < 10)
 	{
-		for (; i < 'z'; i++)
+		for (i = 'a'; i < 'z'; i++)
 			_putchar(i);
 		_putchar('\n');
+		j++;
 	}
 }
