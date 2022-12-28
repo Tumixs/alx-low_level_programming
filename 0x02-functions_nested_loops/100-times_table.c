5c4dd020752afa9c3379b25dcf61e0f5a815d34d
Asere Oluwatumise S <asereoluwatumise@gmail.com>
2022-12-28 21:43:22 +0100

    Project 0x02: task 12
    * Fix checker error
    * Fix betty issue
    * Refactor code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,49 +5,32 @@
  */
 void print_times_table(int n)
 {
-	int i, j, prod, hund, ten, unit;
+	int i, j, prod;
 
+	if (n > 15 || n < 0)
+		return;
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
+		_putchar('0');
+		for (j = 1; j <= n; j++)
 		{
 			prod = i * j;
-			hund = prod / 100;
-			ten = prod / 10;
-			unit = prod % 10;
+			_putchar(',');
+			_putchar(' ');
+			if (prod < 100)
+				_putchar(' ');
+			if (prod < 10)
+				_putchar(' ');
 			if (prod > 99)
 			{
-				_putchar(hund + '0');
-				_putchar(ten % 10 + '0');
-				_putchar(unit + '0');
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(prod / 100 + '0');
+				_putchar(prod / 10 % 10 + '0');
 			}
-			else if (prod > 9)
+			else if (prod > 9 && prod < 100)
 			{
-				_putchar(ten + '0');
-				_putchar(unit + '0');
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
+				_putchar(prod / 10 + '0');
 			}
-			else
-			{
-				_putchar(unit + '0');
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
-			}
+			_putchar(prod % 10 + '0');
 		}
 		_putchar('\n');
 	}
