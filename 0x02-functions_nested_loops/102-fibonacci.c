473038028a6d4454f392b8556b0c3de14e02bd29
Asere Oluwatumise S <asereoluwatumise@gmail.com>
2022-12-28 23:34:13 +0100

    Project 0x02: task 14
    * add file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: returns 0.
+ */
+int main(void)
+{
+	unsigned long int i = 1, j = 2, sum;
+	int n;
+
+	printf("%ld, ", i);
+	printf("%ld, ", j);
+	for (n = 0; n < 48; n++)
+	{
+		sum = i + j;
+		printf("%ld, ", sum);
+		i = j;
+		j = sum;
+	}
+	printf("%ld", sum);
+	printf("\n");
+	return (0);
+}
