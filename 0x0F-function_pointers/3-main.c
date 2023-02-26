cd9ca6b5dbbaa371a37e3a33a33ba1596babeb1b
Asere Oluwatumise S <asereoluwatumise@gmail.com>
2023-02-26 08:49:47 +0100

    Project 0x0F: Added 3-main.c for task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+/* Project 0x0F: task 3_main file*/
+#include "3-calc.h"
+/**
+ * main - program start
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	operation calc;
+	int num1, num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if ((num2 == 0 && *op == '/') || (num2 == 0 && *op == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	calc = get_op_func(argv[2]);
+	printf("%d\n", calc(num1, num2));
+	return (0);
+}
